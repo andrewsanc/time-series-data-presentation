79ca021fccff6bbe5cee942a8b50851deb838196
Andrew Sanchez <druu831@gmail.com>
2024-01-31 08:19:10 -0800

    feat. first commit. new vite project. added tailwind & shadcn

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <Button>Hello World</Button>
+      </div>
+    </>
+  );
+}
